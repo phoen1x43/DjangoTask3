b6b9442f83413addc769335d72bb7686f42dd6a6
phoen1x43 <iliagrigorev5@gmail.com>
2025-03-16 18:02:49 +0300

    Initial commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class Message(models.Model):
-    name = models.CharField(max_length =200)
-    created_at = models.DateTimeField(auto_now_add = True)
+    name = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.timestamp}"
--- myapp/views.py
+++ myapp/views.py
@@ -1,17 +1,16 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Message
-from django.utils import timezone
 
 def hello(request, name):
     Message.objects.create(name=name)
-    return HttpResponse(f"Hello, {name}!")
+    return render(request, "welcome.html", {"name": name})
 
 def stats(request):
-    count = Message.objects.count()
-    return HttpResponse(f"Total greetings: {count}")
+    total_greetings = Message.objects.count()
+    return render(request, "stats.html", {"total_greetings": total_greetings})
 
 def stats_name(request, name):
-    messages = Message.objects.filter(name=name).order_by('-created_at')
-    response = f"Greeting history for {name}:<br>"
-    response += "<br>".join(timezone.localtime(m.created_at).strftime('%Y-%m-%d %H:%M:%S') for m in messages)
-    return HttpResponse(response)
+    greetings = Message.objects.filter(name=name).order_by('-timestamp')
+    return render(request, "stats_name.html", {"name": name, "greetings": greetings})
+
